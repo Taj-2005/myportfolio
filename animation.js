80e5371472e8adbfda691e89b9308a31f391a422
Taj-786 <tajuddinshaik786k@gmail.com>
2025-01-19 04:30:28 +0530

    Taj-2005

--- animation.js
+++ animation.js
@@ -26,10 +26,6 @@ resize();
 step();
 
 window.onresize = resize;
-canvas.onmousemove = onMouseMove;
-canvas.ontouchmove = onTouchMove;
-canvas.ontouchend = onMouseLeave;
-document.onmouseleave = onMouseLeave;
 
 function generate() {
 
